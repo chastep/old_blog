9e95e425bbfcc4cafced08deba5cf3a3d83a275c
Chase Stephens <chasestephens92@gmail.com>
2019-05-14 14:01:11 -0500

    init commit

---
+++ components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+    <div class="home">
+      <nuxt-link to="/">
+        <div class="logo">
+          LOGO HERE
+        </div>
+      </nuxt-link>
+    </div>
+
+    <ul class="social">
+      <li v-for="soc in socials" :key="soc.url">
+        <a :href="soc.url" rel="nofollow">{{soc.name}}</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: undefined,
+      socials: [
+        {name: 'github', url: 'https://github.com/chastep'},
+        {name: 'linkedin', url: 'https://www.linkedin.com/in/chase-stephens/'},
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+header {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  font-size: var(--font-size-2);
+  padding: 1.125rem;
+}
+.home {
+  margin-right: 1.125rem;
+}
+.logo {
+  order: 2;
+  text-align: right;
+}
+.logo > svg {
+  display: inline-block;
+  transition: .33s ease-out;
+}
+.logo > svg:hover {
+  opacity: 1;
+}
+.social {
+  flex: 1;
+  list-style: none;
+  padding: 0;
+  text-align: right;
+  order: 3;
+}
+.social li {
+  display: inline-block;
+}
+.social li:not(:last-child) {
+  margin-right: 0.72rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div class="bio">
+      Hi there, my name is Chase. I'm a tall software engineer currently at
+      <a href="https://www.govisibly.com/">Visibly</a>.
+      I've worked at <a href="https://www.hatchloyalty.com/">Hatch Loyalty</a>
+      and <a href="https://www.colburnhill.com/">Colburn Hill Group</a> before that in similar roles.
+      Before that I worked in the finance sector at
+      <a href="https://www.pnc.com/en/corporate-and-institutional.html?lnksrc=topnav">PNC Bank</a>.
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.bio {
+  text-transform: lowercase;
+}
+</style>
