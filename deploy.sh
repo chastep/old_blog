0efc6747113b06475892da80ce6766e1a8c1d233
Chase Stephens <chasestephens92@gmail.com>
2019-05-14 23:44:53 -0500

    deploy on master i guess

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:chastep/chastep.github.io.git gh-pages
+git push -f git@github.com:chastep/chastep.github.io.git master
 
 cd -
