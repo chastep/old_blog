40fb5b9833afec6f7eb92894f5794b59a5e7776a
Chase Stephens <chasestephens92@gmail.com>
2019-06-06 16:58:08 -0500

    articles page dir with pages, article component, routing

---
+++ components/Article.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="article-container">
+    article
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: undefined,
+      socials: [
+        {name: 'fas fa-info-circle fa-lg', url: '/info' },
+        {name: 'far fa-newspaper fa-lg', url: '/articles' },
+        {name: 'fas fa-code-branch fa-lg', url: 'https://github.com/chastep'},
+        {name: 'fab fa-linkedin fa-lg', url: 'https://www.linkedin.com/in/chase-stephens/'},
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+.article-container {
+  text-align: center;
+  font-size: var(--font-size-4);
+  font-weight: 700;
+  padding: 1.125rem;
+  text-transform: lowercase;
+}
+</style>
